78af90a25b43a45b7c5810905ade5f48e2a9be4d
Dmitry Pavlov <dpavlov@entroforce.ru>
2024-02-27 14:16:41 +0300

    Reflected the change that citeproc is no longer an extenal filter but a module within Pandoc

--- pandoc-demo-espd.bat
+++ pandoc-demo-espd.bat
@@ -1,1 +1,1 @@
-pandoc.exe demo-espd-beginning.md demo-main.md demo-espd-end.md -o demo-espd-raw.docx --filter pandoc-crossref --filter pandoc-citeproc --reference-doc demo-template-espd.docx
+pandoc.exe demo-espd-beginning.md demo-main.md demo-espd-end.md -o demo-espd-raw.docx --filter pandoc-crossref --citeproc --reference-doc demo-template-espd.docx
